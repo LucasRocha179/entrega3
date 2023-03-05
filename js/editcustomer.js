5d0e42da3152dde7ee2c41c317531c3ff43aa23b
F6322452 Lucas Rocha <F6322452@bb.com.br>
2023-03-05 08:16:50 -0300

    Adicionando JS

--- js/editcustomer.js
+++ js/editcustomer.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     $(document).ready(function() {
         $('#telefone').mask('(00) 00000-0000');
-        // $('#cep').mask('00000-000');
     });
 });
 
